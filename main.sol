6158150ee4c8a72ccfe8a26473a88a180a08d4bf
lukasuti751 <lukasuti751@gmail.com>
2026-03-02 13:52:30 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -64,3 +64,69 @@ contract Veg3n is ReentrancyGuard, Pausable, Ownable {
 
     // -------------------------------------------------------------------------
     // ERRORS
+    // -------------------------------------------------------------------------
+
+    error V3G_ZeroAddress();
+    error V3G_InvalidMealId();
+    error V3G_MealNotFound();
+    error V3G_InvalidMealHash();
+    error V3G_InvalidPathTag();
+    error V3G_NotPathCoordinator();
+    error V3G_NotRewardKeeper();
+    error V3G_CompanionPaused();
+    error V3G_ReentrantCall();
+    error V3G_TransferFailed();
+    error V3G_ZeroAmount();
+    error V3G_InsufficientPoints();
+    error V3G_MaxMealsReached();
+    error V3G_ArrayLengthMismatch();
+    error V3G_BatchTooLarge();
+    error V3G_ZeroBatchSize();
+    error V3G_PathNotFound();
+    error V3G_PathNotActive();
+    error V3G_AlreadyOnPath();
+    error V3G_NotOnPath();
+    error V3G_MaxPathsReached();
+    error V3G_InvalidBlockRange();
+    error V3G_InvalidMealType();
+    error V3G_InvalidTargetValue();
+    error V3G_TipNotFound();
+    error V3G_MaxTipsReached();
+    error V3G_NotMealUser();
+    error V3G_MealAlreadyRemoved();
+    error V3G_InvalidLabelHash();
+
+    // -------------------------------------------------------------------------
+    // CONSTANTS
+    // -------------------------------------------------------------------------
+
+    uint256 public constant V3G_BPS_BASE = 10000;
+    uint256 public constant V3G_MAX_MEALS = 3200;
+    uint256 public constant V3G_MAX_PATHS = 110;
+    uint256 public constant V3G_MAX_TIPS = 450;
+    uint256 public constant V3G_MAX_BATCH_MEALS = 42;
+    uint256 public constant V3G_POINTS_SCALE = 1e18;
+    uint256 public constant V3G_COMPANION_SALT = 0x2F7b4E9a1C6d0F3b5E8a2C7d1F4b6E9a3C8d2F5;
+    uint8 public constant V3G_MEAL_BREAKFAST = 1;
+    uint8 public constant V3G_MEAL_LUNCH = 2;
+    uint8 public constant V3G_MEAL_DINNER = 3;
+    uint8 public constant V3G_MEAL_SNACK = 4;
+    uint256 public constant V3G_MAX_DAILY_SNAPSHOTS = 5000;
+    uint256 public constant V3G_BLOCKS_PER_DAY_EST = 7200;
+
+    // -------------------------------------------------------------------------
+    // IMMUTABLES
+    // -------------------------------------------------------------------------
+
+    address public immutable pathCoordinator;
+    address public immutable rewardVault;
+    address public immutable rewardKeeper;
+    uint256 public immutable deployBlock;
+    bytes32 public immutable companionDomain;
+
+    // -------------------------------------------------------------------------
+    // STATE
+    // -------------------------------------------------------------------------
+
+    struct MealLog {
+        address user;
